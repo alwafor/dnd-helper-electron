8d8b7c634f956e6891d56465dea7c03d32a32433
alwafor <alwafor1@gmail.com>
2022-12-20 19:07:46 +0300

    remove unused colors

--- src/frontend/assets/styles/theme.ts
+++ src/frontend/assets/styles/theme.ts
@@ -5,15 +5,8 @@ export const theme = {
     freestyle: 'Freestyle Script, serif',
   },
   colors: {
-    milk: '#ffffff',
-    milkWarm: '#ffe9a8',
-    golden: '#ffe38e',
-    naples: '#ffd862',
-    putty: '#e9ce7d',
-    cold: '#9a9ad9',
-    coldDark: '#7070D8',
-    black075: 'rgba(0,0,0,0.75)',
-    red: '#7a0000',
+    black: 'black',
+    white: 'white',
   },
   screens: {
     sm: '@media(min-width: 640px)',
